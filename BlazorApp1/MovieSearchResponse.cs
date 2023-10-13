9bc0e922ffc073e7018e1be8160f928b850d2e02
Hafsa Arebadi <r0950106@student.thomasmore.be>
2023-10-13 11:06:23 +0200

    First Commit

---
+++ BlazorApp1/MovieSearchResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1
+{
+    public class MovieSearchResponse
+    {
+       
+            public string? Response { get; set; }
+            public int TotalResults { get; set; }
+            public List<Movie>? Search { get; set; }
+        
+
+        public class Movie
+        {
+            public string? Title { get; set; }
+            public string? Year { get; set; }
+            public string? Poster { get; set; }
+        }
+    }
+}
